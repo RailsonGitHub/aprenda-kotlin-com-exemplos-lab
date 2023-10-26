2398d8068e989db6a20bc19e00aab90200c83ff6
RailsonGitHub <78165885+RailsonGitHub@users.noreply.github.com>
2023-10-26 20:31:23 -0300

    Update desafio.kt
    Completando o código do exemplo prático 1 (KOTLIN)!

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,57 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {
+    BASICO, INTERMEDIARIO, AVANCADO
+}
 
-class Usuario
+class ConteudoEducacional(
+    val titulo: String,
+    val descricao: String,
+    val autor: String,
+    val duracao: Int
+)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class Formacao(
+    val nome: String,
+    val nivel: Nivel,
+    val conteudos: MutableList<ConteudoEducacional> = mutableListOf()
+) {
+    val alunosMatriculados = mutableListOf<Aluno>()
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricularAluno(vararg alunos: Aluno) {
+        alunosMatriculados.addAll(alunos)
     }
 }
 
+class Aluno(val nome: String)
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", "Aprenda os conceitos básicos do Kotlin.", "Instrutor João", 45)
+    val conteudo2 = ConteudoEducacional("Java Completo 2023", "Programação Java do Básico ao Avançado.", "Instrutor Railson", 120)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", Nivel.INTERMEDIARIO)
+    formacaoKotlin.adicionarConteudo(conteudo1)
+    formacaoKotlin.adicionarConteudo(conteudo2)
+
+    val aluno1 = Aluno("José")
+    val aluno2 = Aluno("Maria")
+
+    formacaoKotlin.matricularAluno(aluno1, aluno2)
+
+    println("FORMAÇÃO: ${formacaoKotlin.nome}")
+    println("NÍVEL: ${formacaoKotlin.nivel}")
+    println("DURAÇÃO DO CURSO: ${formacaoKotlin.conteudos.sumBy { it.duracao }} minutos") // Calcula a duração total dos conteúdos
+    println("CONTEÚDOS EDUCACIONAIS:")
+    for (conteudo in formacaoKotlin.conteudos) {
+        println("- ${conteudo.titulo} (Autor: ${conteudo.autor}, Duração: ${conteudo.duracao} minutos)")
+    }
+
+    println("Alunos matriculados:")
+    for (aluno in formacaoKotlin.alunosMatriculados) {
+        println("- ${aluno.nome}")
+    }
 }
